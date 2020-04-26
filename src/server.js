6e02f3910ea384de0a59123e2124edb94bdaa636
pekerleke <mrtnsalem@gmail.com>
2020-04-26 17:14:50 -0300

    update serverjs

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/portfolio'));
+app.use(express.static('/app/dist/portfolio'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
+    res.sendFile(path.join('/app/dist/portfolio/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
