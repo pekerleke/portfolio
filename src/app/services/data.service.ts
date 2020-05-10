7e612a96bcea4ba5fd9a713c371f34f11686f8f3
pekerleke <mrtnsalem@gmail.com>
2020-05-10 03:19:05 -0300

    change aptitudes style

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -37,32 +37,56 @@ export class DataService {
       {
         name: 'Angular 2+',
         image_url: '/assets/images/aptitudes/angular.png',
-        url: 'https://angular.io/'
+        url: 'https://angular.io/',
+        percentage: '80'
       },
       {
         name: 'Nginx',
         image_url: '/assets/images/aptitudes/nginx.png',
-        url: 'https://www.nginx.com/'
+        url: 'https://www.nginx.com/',
+        percentage: '45'
       },
       {
         name: 'Openresty',
         image_url: '/assets/images/aptitudes/openresty.png',
-        url: 'https://openresty.org/en/'
+        url: 'https://openresty.org/en/',
+        percentage: '40'
       },
       {
         name: 'Lua',
         image_url: '/assets/images/aptitudes/lua.png',
-        url: 'https://www.lua.org/'
+        url: 'https://www.lua.org/',
+        percentage: '35'
       },
       {
         name: 'JavaScript',
         image_url: '/assets/images/aptitudes/js.png',
-        url: 'https://developer.mozilla.org/es/docs/Web/JavaScript'
+        url: 'https://developer.mozilla.org/es/docs/Web/JavaScript',
+        percentage: '80'
       },
       {
         name: 'scss',
         image_url: '/assets/images/aptitudes/sass.png',
-        url: 'https://sass-lang.com/'
+        url: 'https://sass-lang.com/',
+        percentage: '90'
+      },
+      {
+        name: 'Json',
+        image_url: '/assets/images/aptitudes/json.png',
+        url: '#',
+        percentage: '90'
+      },
+      {
+        name: 'Gimp',
+        image_url: '/assets/images/aptitudes/gimp.png',
+        url: '#',
+        percentage: '70'
+      },
+      {
+        name: 'Python',
+        image_url: '/assets/images/aptitudes/python.png',
+        url: '#',
+        percentage: '35'
       },
     ]
   };
